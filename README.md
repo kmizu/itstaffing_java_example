bf4409c8afd0b14b93efa759d4dc2e81a1f36b78
Kota Mizushima <mizukota@gmail.com>
2019-02-12 01:51:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## itstaffing-java-example
+
+http://www.itstaffing.jp/engineer/entry/event_20190213
+
+で使うサンプルのJavaプログラムです。
---
+++ src/main/java/com/github/kmizu/itstaffing_java_example/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.github.kmizu.itstaffing_java_example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
